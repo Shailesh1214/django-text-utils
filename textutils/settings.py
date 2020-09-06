f5fd5df724018ce864721ac3b703d188dc6170c2
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-09-06 20:03:20 +0530

    Project pipline

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')-a__lkplpqu(nglnv#t4bgsu8os#6ve3rcb0--^hhl!=#-ko7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- textutils/views.py
+++ textutils/views.py
@@ -1,1 +1,20 @@
 # I have created this files - shailesh
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("new line remover")
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount")
+
