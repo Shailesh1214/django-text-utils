d90cab44ba81cec8b1a9276d4174a7336492faed
Shailesh1214 <60460179+Shailesh1214@users.noreply.github.com>
2020-09-09 08:21:46 +0530

    functions

--- textutils/views.py
+++ textutils/views.py
@@ -10,9 +10,14 @@ def index(request):
 def analyze(request):
     #get the text
     djtext = request.GET.get('text','default')
+    # check checkbox value
     removepunc = request.GET.get('removepunc','off')
-    print(removepunc)
-    print(djtext)
+    fullcaps = request.GET.get('fullcaps','off')
+    newlineremover = request.GET.get('newlineremover','off')
+    spaceremover = request.GET.get('spaceremover','off')
+    charcount = request.GET.get('charcount','off')
+
+    # check which check box is on
     if removepunc == "on":
         analyzed = djtext
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_'''
@@ -24,6 +29,38 @@ def analyze(request):
         params={'purpose':'Remove Punctuations','analyzed_text':analyzed}
     #analyze the text
         return render(request,'analyze.html',params)
+    elif(fullcaps == "on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params={'purpose':'Changed to Uppercase','analyzed_text':analyzed}
+    #analyze the text
+        return render(request,'analyze.html',params)
+    elif(newlineremover == "on"):
+         analyzed = ""
+         for char in djtext:
+            if char != "\n":
+                analyzed = analyzed + char
+         params={'purpose':'Removed NewLines','analyzed_text':analyzed}
+    #analyze the text
+         return render(request,'analyze.html',params)
+    elif(spaceremover == "on"):
+         analyzed = ""
+         for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index +1] == " "):
+                analyzed = analyzed + char
+         params={'purpose':'Extra Space Remover','analyzed_text':analyzed}
+    #analyze the text
+         return render(request,'analyze.html',params)
+    elif(charcount == "on"):
+         analyzed = ""
+         count = 0
+         for  char in enumerate(djtext):
+                count = count + 1
+                analyzed = count
+         params={'purpose':'The Number of character in your string','analyzed_text':analyzed}
+    #analyze the text
+         return render(request,'analyze.html',params)     
     else:
         return HttpResponse("Error")
 
